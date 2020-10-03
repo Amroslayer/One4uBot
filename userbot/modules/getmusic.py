5810fc0c764f725088aec6dee12705e4ffd05bd3
Amroslayer <55046247+Amroslayer@users.noreply.github.com>
2020-10-03 12:01:56 +0700

    remove @woomaibot chat

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -47,47 +47,6 @@ def getmusicvideo(cat):
     os.system(command)
 
 
-@register(outgoing=True, pattern=r"^\.songn (?:(now)|(.*) - (.*))")
-async def _(event):
-    if event.fwd_from:
-        return
-    if event.pattern_match.group(1) == "now":
-        playing = User(LASTFM_USERNAME, lastfm).get_now_playing()
-        if playing is None:
-            return await event.edit("`Error: No current scrobble found.`")
-        artist = playing.get_artist()
-        song = playing.get_title()
-    else:
-        artist = event.pattern_match.group(2)
-        song = event.pattern_match.group(3)
-    track = str(artist) + " - " + str(song)
-    chat = "@WooMaiBot"
-    link = f"/netease {track}"
-    await event.edit("`Searching...`")
-    try:
-        async with bot.conversation(chat) as conv:
-            await asyncio.sleep(2)
-            await event.edit("`Downloading...Please wait`")
-            try:
-                msg = await conv.send_message(link)
-                response = await conv.get_response()
-                respond = await conv.get_response()
-                """- don't spam notif -"""
-                await bot.send_read_acknowledge(conv.chat_id)
-            except YouBlockedUserError:
-                await event.reply("```Please unblock @WooMaiBot and try again```")
-                return
-            await event.edit("`Sending Your Music...`")
-            await asyncio.sleep(3)
-            await bot.send_file(event.chat_id, respond)
-        await event.client.delete_messages(
-            conv.chat_id, [msg.id, response.id, respond.id]
-        )
-        await event.delete()
-    except TimeoutError:
-        return await event.edit("`Error: `@WooMaiBot` is not responding!.`")
-
-
 @register(outgoing=True, pattern=r"^\.songl(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
@@ -227,9 +186,7 @@ async def _(event):
 
 CMD_HELP.update(
     {
-        "getmusic": ".songn <Artist - Song Title>"
-        "\nUsage: Download music by name (@WooMaiBot)"
-        "\n\n.songl <Spotify/Deezer Link>"
+        "getmusic": ".songl <Spotify/Deezer Link>"
         "\nUsage: Download music by link (@MusicsHunterBot)"
         "\n\n.songf <Artist - Song Title>"
         "\nUsage: Download music by name (@SpotifyMusicDownloaderBot)"
